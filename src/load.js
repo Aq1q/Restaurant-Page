703f3c5bf18875e975427c47cc2777bd27392bcc
Michał Salabura <micheu334@gmail.com>
2022-10-23 18:10:04 +0200

    Create header style using js

--- src/load.js
+++ src/load.js
@@ -1,25 +1,39 @@
+function createListElement (id, text) {
+    const element = document.createElement('li')
+    element.id = id;
+    element.classList.add(`menu`);
+    element.innerText = text;
+
+    element.style.fontSize = '1.5rem';
+    element.style.listStyle = 'none';
+    element.style.border = '1px solid gray';
+    element.style.color = '#1C1A29';
+    element.style.padding = '0 4px 4px 4px';
+    element.style.backgroundColor = 'rebeccapurple';
+
+    return element;
+}
+
 function createHeader () {
     const header = document.createElement('header');
+    header.style.width = '100%';
+    header.style.height = '50px';
+    header.style.backgroundColor = 'lightcoral';
+    header.style.display = 'flex';
+    header.style.justifyContent = 'center';
+    header.style.alignItems = 'center';
+
     const ul = document.createElement('ul');
-    const homeButton = document.createElement('li');
-    const menuButton = document.createElement('li');
-    const contactButton = document.createElement('li');
+    ul.style.alignItems = 'center';
+    ul.style.display = 'flex';
+    ul.style.gap = 'max(100px)';
+    ul.style.justifyContent = 'center';
+    ul.style.margin = '0';
+
+    ul.appendChild(createListElement('home', 'Home'));
+    ul.appendChild(createListElement('menu', 'Menu'));
+    ul.appendChild(createListElement('contact','Contact'));
     
-    homeButton.id = 'home';
-    homeButton.classList.add('menu');
-    homeButton.innerText = 'Home';
-
-    menuButton.id = 'menu';
-    menuButton.classList.add('menu');
-    menuButton.innerText = 'Menu';
-
-    contactButton.id = 'contact';
-    contactButton.classList.add('menu');
-    contactButton.innerText = 'Contact';
-
-    ul.appendChild(homeButton);
-    ul.appendChild(menuButton);
-    ul.appendChild(contactButton);
     header.appendChild(ul);
 
     return header;
